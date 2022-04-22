f63c8a4404780bef192fb88616aedb31ad827030
tangyanbiao <tangyanbiao@xeno.com>
2022-04-22 11:19:26 +0800

    fix: ts config error

--- ts.js
+++ ts.js
@@ -1,8 +1,6 @@
 const eslintrc = require('./.eslintrc.js')
 
 eslintrc.root = true
-eslintrc.parserOptions.project = 'tsconfig.json'
-eslintrc.parserOptions.tsconfigRootDir = __dirname
 eslintrc.parser = '@typescript-eslint/parser'
 eslintrc.plugins = ['@typescript-eslint']
 eslintrc.extends = [
