50a58cebf96a64bd5ab7c16c2ddee584cd0fee78
tangyanbiao <tangyanbiao@xeno.com>
2022-09-21 18:18:47 +0800

    feat: ts 增加额外检查

--- ts.js
+++ ts.js
@@ -18,5 +18,9 @@ eslintrc.rules = {
             caughtErrorsIgnorePattern: '^_',
         },
     ],
+    '@typescript-eslint/interface-name-prefix': 'off',
+    '@typescript-eslint/explicit-function-return-type': 'off',
+    '@typescript-eslint/explicit-module-boundary-types': 'off',
+    '@typescript-eslint/no-explicit-any': 'off',
 }
 module.exports = eslintrc
